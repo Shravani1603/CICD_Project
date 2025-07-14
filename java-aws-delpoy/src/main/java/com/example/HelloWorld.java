e8b3bba02d0e5c69f7e92a9f10b1afb8fe9a0858
Shravani1603 <149895354+Shravani1603@users.noreply.github.com>
2025-07-14 12:22:25 +0530

    Update HelloWorld.java

--- java-aws-delpoy/src/main/java/com/example/HelloWorld.java
+++ java-aws-delpoy/src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins Deployment on AWS!");
+        System.out.println("Hello shravani, Jenkins Deployment on AWS!");
     }
 }
